5fc77754ba0963e2f6e45c12b8a0cd76c9016bcc
jarangol <jarangol@3ed1721f-7d64-4264-9eb4-a61ef7991979>
2016-05-12 14:58:32 +0000

    Intentando leer archivo

--- TSP_FuerzaBruta.cpp
+++ TSP_FuerzaBruta.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
 int main(int argc, char* argv[]){
   int id;
   double x,y;
-  string name, linea;
-  
+  string name, linea,token;
+
   
   for(int i=0;i<argc;i++){
-    ifstream archivo("argv[i]");
-    
-    while(archivo.getLine(linea)){
-      cout<<linea<<endl;
 
+    ifstream archivo(argv[i]);
+    //archivo.close();
+    
+    while(getline(archivo,linea)){
+      
+      cout<<"linea: "<<linea<<endl;
+    
+      istringstream cadena(linea);  
+
+      while(getline(cadena,token, ',')){
+	cout<<"Token: "<<token<<endl;
+      
+      }
     }
 
   }
+
+  
   return 0;
 }
